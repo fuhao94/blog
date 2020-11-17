5b7d7fc84956fcbde1a5890d521ffe900dbcc444
张福浩 <zhangfuhao@mininglamp.com>
2020-11-17 11:55:28 +0800

    添加下导航栏

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -52,6 +52,112 @@ module.exports = {
     ],
     sidebar: {
       '/docs/theme-reco/': ['', 'theme', 'plugin', 'api'],
+      "/blogs/": [
+        {
+          title: '小工具',
+          children: [
+            'tools/tools',
+          ]
+        },
+        {
+          title: 'HTML && CSS',
+          children: [
+            'css/选择器',
+            'css/position',
+            'css/太极图',
+            'css/垂直居中布局'
+          ]
+        },
+        {
+          title: 'JavaScript',
+          children: [
+            'javascript/ES6',
+            'javascript/class',
+            'javascript/promise',
+            'javascript/extend',
+            'javascript/closure',
+            'javascript/防抖节流',
+            'javascript/settimeout',
+            'javascript/prototype',
+            'javascript/curry',
+            'javascript/深浅拷贝',
+            'javascript/constructor',
+            'javascript/event',
+            'javascript/数组操作',
+            'javascript/判断数据类型',
+            'javascript/immutability',
+            'javascript/let-const',
+            'javascript/设计模式/设计模式',
+            'javascript/gc',
+            'javascript/spa-mpa',
+            'javascript/img-lazy-load',
+            'javascript/ssr',
+            'javascript/输入URL发生了什么',
+          ]
+        },
+        {
+          title: 'TypeScript',
+          children: [
+            'typescript/interface和type区别',
+          ]
+        },
+        {
+          title: 'Vue',
+          children: [
+            'vue/router',
+            'vue/first-screen',
+            'vue/binding/observer',
+            'vue/vuex/vuex',
+            'vue/virtual-dom',
+            'vue/router',
+            'vue/dynamic-loading',
+            'vue/computed-watch',
+          ]
+        },
+        {
+          title: 'React',
+          children: [
+            'react/原理',
+            'react/使用',
+            'react/进阶',
+            'react/hooks',
+          ]
+        },
+        {
+          title: 'Webpack',
+          children: [
+            'webpack/HMR',
+            'webpack/tips',
+            'webpack/process',
+          ]
+        },
+        {
+          title: '网络',
+          children: [
+            'network/http',
+            'network/cross-domain',
+            'network/request-headers',
+            'network/get-post',
+            'network/3-4',
+          ]
+        },
+        {
+          title: '算法',
+          children: [
+            'algorithm/promise',
+            'algorithm/高频',
+          ]
+        },
+        {
+          title: '总结',
+          children: [
+            'summary/direction',
+            'summary/fixed',
+            'summary/todo',
+            'summary/nEqual-serving',
+          ]
+        },
+      ]
     },
     type: 'blog',
     valineConfig: {
