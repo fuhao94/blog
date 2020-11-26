33ae30e578f6703236918abd3b57f366d96e55dc
张福浩 <zhangfuhao@mininglamp.com>
2020-11-26 14:20:49 +0800

    提取下menus

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,9 @@
+const sidebar = require('./menu');
+
 module.exports = {
   base: '/blog/',
   title: '博客',
-  author: 'zhangfuhao',
+  author: '隔壁老张头',
   description: 'my blog',
   dest: 'public',
   head: [
@@ -22,6 +24,7 @@ module.exports = {
   ],
   theme: 'reco',
   themeConfig: {
+    sidebar,
     subSidebar: 'auto',
     nav: [
       {
@@ -50,115 +53,6 @@ module.exports = {
         icon: 'reco-github',
       },
     ],
-    sidebar: {
-      '/docs/theme-reco/': ['', 'theme', 'plugin', 'api'],
-      "/blogs/": [
-        {
-          title: '小工具',
-          children: [
-            'tools/tools',
-          ]
-        },
-        {
-          title: 'HTML && CSS',
-          children: [
-            'css/选择器',
-            'css/position',
-            'css/太极图',
-            'css/垂直居中布局'
-          ]
-        },
-        {
-          title: 'JavaScript',
-          children: [
-            'javascript/ES6',
-            'javascript/class',
-            'javascript/promise',
-            'javascript/extend',
-            'javascript/closure',
-            'javascript/防抖节流',
-            'javascript/settimeout',
-            'javascript/prototype',
-            'javascript/curry',
-            'javascript/深浅拷贝',
-            'javascript/constructor',
-            'javascript/event',
-            'javascript/数组操作',
-            'javascript/判断数据类型',
-            'javascript/immutability',
-            'javascript/let-const',
-            'javascript/设计模式/设计模式',
-            'javascript/gc',
-            'javascript/spa-mpa',
-            'javascript/img-lazy-load',
-            'javascript/ssr',
-            'javascript/输入URL发生了什么',
-          ]
-        },
-        {
-          title: 'TypeScript',
-          children: [
-            'typescript/interface和type区别',
-          ]
-        },
-        {
-          title: 'Vue',
-          children: [
-            'vue/router',
-            'vue/first-screen',
-            'vue/binding/observer',
-            'vue/vuex/vuex',
-            'vue/virtual-dom',
-            'vue/router',
-            'vue/dynamic-loading',
-            'vue/computed-watch',
-          ]
-        },
-        {
-          title: 'React',
-          children: [
-            'react/原理',
-            'react/使用',
-            'react/进阶',
-            'react/hooks',
-          ]
-        },
-        {
-          title: 'Webpack',
-          children: [
-            'webpack/HMR',
-            'webpack/tips',
-            'webpack/process',
-          ]
-        },
-        {
-          title: '网络',
-          children: [
-            'network/http',
-            'network/cross-domain',
-            'network/request-headers',
-            'network/get-post',
-            'network/3-4',
-          ]
-        },
-        {
-          title: '算法',
-          children: [
-            'algorithm/promise',
-            'algorithm/高频',
-          ]
-        },
-        {
-          title: '总结',
-          children: [
-            'summary/direction',
-            'summary/fixed',
-            'summary/todo',
-            'summary/nEqual-serving',
-          ]
-        },
-      ]
-    },
     type: 'blog',
     valineConfig: {
       appId: 'Voq2t3M8hYFqYaBsCzctqnTz-gzGzoHsz',// your appId
@@ -187,7 +81,7 @@ module.exports = {
     search: true,
     searchMaxSuggestions: 10,
     lastUpdated: '上次更新时间',
-    author: 'zhangfuhao',
+    author: '隔壁老张头',
     authorAvatar: '/avatar.jpeg',
     record: 'xxxx',
     startYear: '2020',
@@ -200,7 +94,7 @@ module.exports = {
     '@vuepress-reco/vuepress-plugin-kan-ban-niang': {
       theme: ['koharu', 'miku', 'z16', 'blackCat'],
       messages: {
-        welcome: '欢迎来到zhangfuhao的博客',
+        welcome: '欢迎来到 隔壁老张头 的博客',
         home: '心里的花，我想要带你回家。',
         theme: '好吧帅帅，希望你能喜欢我的其他小伙伴。',
         close: '你知道我喜欢吃什么吗？痴痴地望着你。'
