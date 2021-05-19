6239e3d00fc7fb7956f454e00e9bc5e956f65f42
zhangfuhao <919155932@qq.com>
2021-05-19 14:58:57 +0800

    每日一题：二维数组中的查找

---
+++ blogs/LeetCode/二维数组中的查找.js
@@ -0,0 +1,78 @@
+//在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个
+//整数，判断数组中是否含有该整数。
+//
+//
+//
+// 示例:
+//
+// 现有矩阵 matrix 如下：
+//
+//
+//[
+//  [1,   4,  7, 11, 15],
+//  [2,   5,  8, 12, 19],
+//  [3,   6,  9, 16, 22],
+//  [10, 13, 14, 17, 24],
+//  [18, 21, 23, 26, 30]
+//]
+//
+//
+// 给定 target = 5，返回 true。
+//
+// 给定 target = 20，返回 false。
+//
+//
+//
+// 限制：
+//
+// 0 <= n <= 1000
+//
+// 0 <= m <= 1000
+//
+//
+//
+// 注意：本题与主站 240 题相同：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/
+// Related Topics 数组 双指针
+// 👍 327 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var findNumberIn2DArray = function (matrix, target) {
+  if (matrix == null || matrix.length === 0 || matrix[0].length === 0) {
+    return false;
+  }
+
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+  let i = 0;
+  let j = columns - 1;
+
+  while (i < rows && j >= 0) {
+    const num = matrix[i][j];
+    if (num === target) {
+      return true;
+    } else if (num > target) {
+      j--;
+    } else {
+      i++
+    }
+  }
+
+  return false;
+};
+
+const test = [
+  [1, 4, 7, 11, 15],
+  [2, 5, 8, 12, 19],
+  [3, 6, 9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+];
+
+console.log(findNumberIn2DArray(test, 5)); // true
+console.log(findNumberIn2DArray(test, 20)); // false
